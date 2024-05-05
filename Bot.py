4b35e537978ef9fc613559bc14563393f37d9284
Sans512 <168572655+Sans512@users.noreply.github.com>
2024-05-05 21:56:40 +0530

    Bot.py

--- Bot.py
+++ Bot.py
@@ -7,8 +7,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
-
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+bot = commands.Bot(command_prefix='!',intents=intents)
 
 @bot.event
 async def on_ready():
